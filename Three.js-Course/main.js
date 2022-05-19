ef193ec9853aecb200a77407f3310d4e18b22b84
tataqt <tata_98@ukr.net>
2022-05-19 12:43:01 +0300

    create scene, camera and renderer

--- Three.js-Course/main.js
+++ Three.js-Course/main.js
@@ -1,6 +1,11 @@
-import './style.css'
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1,1000);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(innerWidth, innerHeight);
+
+document.body.appendChild(renderer.domElement)
+console.log(scene, camera, renderer);
