8deec3f649db55db63523842e37ab6e2e661c486
tataqt <tata_98@ukr.net>
2022-05-19 17:05:04 +0300

    animate and rotate box

--- Three.js-Course/main.js
+++ Three.js-Course/main.js
@@ -2,10 +2,11 @@ import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import './style.css';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1,1000);
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
 
 document.body.appendChild(renderer.domElement);
 
@@ -23,4 +24,11 @@ scene.add(mesh);
 
 camera.position.z = 5;
 
-renderer.render(scene, camera);
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+}
+
+animate();
