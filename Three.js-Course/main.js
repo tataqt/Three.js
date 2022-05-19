b999cbd9b23573ee9afa9202a0ea9999f2b1f27c
tataqt <tata_98@ukr.net>
2022-05-19 17:28:53 +0300

    add light

--- Three.js-Course/main.js
+++ Three.js-Course/main.js
@@ -4,30 +4,26 @@ import './style.css';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
+const light = new THREE.DirectionalLight(
+  0xffffff, 1
+);
 
 renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
 
 camera.position.z = 5;
+light.position.set(0, 0, 1);
+
+scene.add(light);
 
 document.body.appendChild(renderer.domElement);
-
-const boxGeometry = new THREE.BoxGeometry(
-  1, 1, 1
-);
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00FF00
-});
-const mesh = new THREE.Mesh(boxGeometry, material);
-
-scene.add(mesh);
 
 const planeGeometry = new THREE.PlaneGeometry(
   5, 5, 10, 10
 );
-const planeMaterial = new THREE.MeshBasicMaterial({
+const planeMaterial = new THREE.MeshPhongMaterial({
   color: 0xff0000,
-  side: THREE.Double.Side
+  side: THREE.DoubleSide
 });
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 
@@ -36,8 +32,6 @@ scene.add(planeMesh)
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.01;
 }
 
 animate();
